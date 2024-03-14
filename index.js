49fb93d1dd1da72a5f388784a910d6bc805b5d95
avulaprashanthi04 <99384706+avulaprashanthi04@users.noreply.github.com>
2024-03-14 20:46:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Page Title</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  margin: 0;
-}
+var http = require('http');
 
-/* Style the header */
-.header {
-  padding: 100px;
-  text-align: center;
-  background: #1abc9c;
-  color: White;
-}
-
-/* Increase the font size of the h1 element */
-.header h1 {
-  font-size: 60px;
-}
-</style>
-</head>
-<body>
-
-<div class="header">
-  <h1>Thankgod its Sunday!!!!</h1>
- <p id="demo">let's sleep for few more minutes :)</p>
-
-<button type="button" onclick="document.getElementById('demo').style.fontSize='35px'">Good Night!</button>
-</div>
-
-</body>
-</html>
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hey guyss...!!!!!
+			You are listening to my Jenkins class'); //write a response to the client
+  res.end(); //end the response
+}).listen(8000); //the server object listens on port 8000
