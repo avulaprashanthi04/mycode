6f85649e6d7e096eeda1d2c638f25cfb93cef91f
avulaprashanthi04 <99384706+avulaprashanthi04@users.noreply.github.com>
2024-03-14 20:34:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Page Title</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+/* Style the header */
+.header {
+  padding: 100px;
+  text-align: center;
+  background: #1abc9c;
+  color: White;
+}
+
+/* Increase the font size of the h1 element */
+.header h1 {
+  font-size: 60px;
+}
+</style>
+</head>
+<body>
+
+<div class="header">
+  <h1>Thankgod its Sunday!!!!</h1>
+ <p id="demo">let's sleep for few more minutes :)</p>
+
+<button type="button" onclick="document.getElementById('demo').style.fontSize='35px'">Good Night!</button>
+</div>
+
+</body>
+</html>
